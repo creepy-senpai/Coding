8f772f7ebd7838e3630c5202caf0f44a9a8fb2e7
Karan <karankadayat55@gmail.com>
2025-09-02 21:58:28 +0530

    wrong code hai

---
+++ calci.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main()
+{
+        /*for(int x=1;x<101;x++)
+            {
+            if(x%2==!0)
+                 printf("%d\n",x);
+            }*/
+          /* int i=1;
+           while(i<101)
+           {
+            if(i%2==0)
+            printf("%d\n",i);
+            i++;
+           }*/
+          int i=1;
+          do
+        {
+            if(i%2==0)
+            {
+            printf("%d\n",i);
+            i++;
+            }
+         }
+          while(i<101);
+          
+}
